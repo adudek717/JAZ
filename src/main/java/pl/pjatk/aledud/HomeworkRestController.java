ecf1b4054fcefb05a0ea057e0b2d8a44a1298e0c
adudek717 <a.dudek717@gmail.com>
2021-04-19 00:46:38 +0200

    Removed redundant lines

--- src/main/java/pl/pjatk/aledud/HomeworkRestController.java
+++ src/main/java/pl/pjatk/aledud/HomeworkRestController.java
@@ -8,35 +8,28 @@ package pl.pjatk.aledud;
 @RequestMapping("/homework")
 public class HomeworkRestController {
 
-    private MyClassOne myClassOne;
-
-    public HomeworkRestController(MyClassOne myClassOne) {
-        this.myClassOne = myClassOne;
-    }
-
-
     @GetMapping("/{someValue}")
     public ResponseEntity<String> getGreeting(@PathVariable("someValue") String someValue) {
         return ResponseEntity.ok(someValue);
     }
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<String> getRequestGreeting(@RequestParam String someValue) {
         return ResponseEntity.ok(someValue);
     }
 
-    @PutMapping("/put/{someValue}")
+    @PutMapping("/{someValue}")
     public ResponseEntity<MyClassOne> putGreeting(@PathVariable String someValue, @RequestBody MyClassOne myClassOne) {
         myClassOne.setName(someValue);
         return ResponseEntity.ok(myClassOne);
     }
 
-    @PostMapping("/post")
+    @PostMapping
     public ResponseEntity<MyClassOne> postGreeting(@RequestBody MyClassOne myClassOne) {
         return ResponseEntity.ok(myClassOne);
     }
 
-    @DeleteMapping("/delete/{someValue}")
+    @DeleteMapping("/{someValue}")
     public ResponseEntity<HttpStatus> deleteGreeting(@PathVariable("someValue") String someValue) {
         return ResponseEntity.ok(HttpStatus.OK);
     }
